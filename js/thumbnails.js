9af7344e5e60eedeb9a803f2b373f61374f2baaf
Polina <polinakuzmina2904@gmail.com>
2023-11-26 20:13:41 +0500

    рефакторинг

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,15 +1,19 @@
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesList = document.querySelector('.pictures');
-const picturesListFragment = document.createDocumentFragment();
+
+const createThumbnail = ({url, description, likes, comments}) => {
+  const thumbnail = pictureTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  return thumbnail;
+};
 
 const renderThumbnails = (photos) => {
-  photos.forEach(({url, description, likes, comments}) => {
-    const thumbnail = pictureTemplate.cloneNode(true);
-    thumbnail.querySelector('.picture__img').src = url;
-    thumbnail.querySelector('.picture__img').alt = description;
-    thumbnail.querySelector('.picture__likes').textContent = likes;
-    thumbnail.querySelector('.picture__comments').textContent = comments.length;
-    picturesListFragment.appendChild(thumbnail);
+  const picturesListFragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    picturesListFragment.appendChild(createThumbnail(photo));
   });
   picturesList.appendChild(picturesListFragment);
 };
