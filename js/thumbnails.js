ae4d68e809e3dd8891614e2387e37f5f2daa4e80
Polina <polinakuzmina2904@gmail.com>
2023-11-28 21:40:12 +0500

    подключение файла

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,8 +1,24 @@
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const picturesList = document.querySelector('.pictures');
+import { showFullsizePicture } from './render-big-picture.js';
 
-const createThumbnail = ({url, description, likes, comments}) => {
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+let pictures = null;
+
+const onPicturesContainerClick = (evt) => {
+  const targetElement = evt.target.closest('.picture');
+  if (targetElement) {
+    const id = targetElement.dataset.pictureId;
+    const [thumbnail] = pictures.filter((picture) => picture.id === +id);
+    //console.log(thumbnail);
+    showFullsizePicture(thumbnail);
+  }
+};
+
+const createThumbnail = ({url, description, likes, comments, id}) => {
   const thumbnail = pictureTemplate.cloneNode(true);
+
+  thumbnail.dataset.pictureId = id;
+  //console.log(url);
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
@@ -10,12 +26,39 @@ const createThumbnail = ({url, description, likes, comments}) => {
   return thumbnail;
 };
 
-const renderThumbnails = (photos) => {
+const renderThumbnails = (data) => {
+  pictures = data.slice();
   const picturesListFragment = document.createDocumentFragment();
-  photos.forEach((photo) => {
-    picturesListFragment.appendChild(createThumbnail(photo));
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    picturesListFragment.appendChild(thumbnail);
   });
-  picturesList.appendChild(picturesListFragment);
+
+  picturesContainer.appendChild(picturesListFragment);
+  picturesContainer.addEventListener('click', onPicturesContainerClick);
 };
 
 export {renderThumbnails};
+
+// const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+// const picturesList = document.querySelector('.pictures');
+
+// const createThumbnail = ({url, description, likes, comments}) => {
+//   const thumbnail = pictureTemplate.cloneNode(true);
+//   thumbnail.querySelector('.picture__img').src = url;
+//   thumbnail.querySelector('.picture__img').alt = description;
+//   thumbnail.querySelector('.picture__likes').textContent = likes;
+//   thumbnail.querySelector('.picture__comments').textContent = comments.length;
+//   return thumbnail;
+// };
+
+// const renderThumbnails = (photos) => {
+//   const picturesListFragment = document.createDocumentFragment();
+//   photos.forEach((photo) => {
+//     picturesListFragment.appendChild(createThumbnail(photo));
+//   });
+//   picturesList.appendChild(picturesListFragment);
+// };
+
+// export {renderThumbnails};
+
